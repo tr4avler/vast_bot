fa9c09b360de5e424520b3e8aac44ebb4280fa62
tr4avler <147596084+tr4avler@users.noreply.github.com>
2023-10-22 15:43:26 +0200

    Update bot_3.py

--- bot_3.py
+++ bot_3.py
@@ -49,19 +49,24 @@ def test_api_connection():
     except Exception as e:
         logging.error(f"Error connecting to API: {e}")
 
-def check_balance():
-    url = f"https://console.vast.ai/api/v0/accounts/me/?api_key={api_key}"
-    response = requests.get(url)
+def get_user_details():
+    url = f"https://console.vast.ai/api/v0/users/current?api_key={api_key}"
+    headers = {'Accept': 'application/json'}
+    response = requests.get(url, headers=headers)
     if response.status_code == 200:
-        data = response.json()
-        return data.get('balance', 0)
-    return 0
+        try:
+            return response.json()
+        except Exception as e:
+            logging.error(f"Failed to parse JSON from user details API response: {e}. Response text: {response.text}")
+            return {}
+    else:
+        logging.error(f"User details API returned an error. Status code: {response.status_code}. Response: {response.text}")
+        return {}
 
 def search_gpu():
     url = "https://console.vast.ai/api/v0/bundles/"
     headers = {'Accept': 'application/json'}
     response = requests.post(url, headers=headers, json=SEARCH_CRITERIA)
-    
     if response.status_code == 200:
         logging.info("Initial offers check went successfully.")
         try:
@@ -87,9 +92,12 @@ def place_order(offer_id):
 # Test API connection first
 test_api_connection()
 
-# Log initial balance
-initial_balance = check_balance()
-logging.info(f"Starting with a balance of ${initial_balance:.2f}")
+# Fetch and log user details
+user_details = get_user_details()
+if user_details:
+    logging.info(f"User '{user_details.get('username', 'Unknown')}' initialized with a balance of ${user_details.get('balance', 'Unknown')}")
+else:
+    logging.error("Failed to fetch user details. Check API connectivity and credentials.")
 
 # Main Loop
 last_balance_log_time = time.time()
