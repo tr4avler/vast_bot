f86f3848402655f6b560f064f9c4c88547421700
tr4avler <147596084+tr4avler@users.noreply.github.com>
2023-10-22 18:08:52 +0200

    Update bot_3.py

--- bot_3.py
+++ bot_3.py
@@ -2,7 +2,7 @@ import requests
 import logging
 import time
 
-# Constantsqq
+# ConstantsFINAL
 API_KEY_FILE = 'api_key.txt'
 CHECK_INTERVAL = 120  # 2 minutes
 BALANCE_LOG_INTERVAL = 300  # 5 minutes
