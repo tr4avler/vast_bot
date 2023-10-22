ae6efc84d2034bfb75a73078c4e2ed41dd408f5b
tr4avler <147596084+tr4avler@users.noreply.github.com>
2023-10-22 17:13:28 +0200

    Update bot_3.py

--- bot_3.py
+++ bot_3.py
@@ -2,7 +2,7 @@ import requests
 import logging
 import time
 
-# Constants123
+# Constantsw
 API_KEY_FILE = 'api_key.txt'
 CHECK_INTERVAL = 120  # 2 minutes
 BALANCE_LOG_INTERVAL = 300  # 5 minutes
@@ -67,7 +67,8 @@ def search_gpu():
     headers = {'Accept': 'application/json'}
     response = requests.post(url, headers=headers, json=SEARCH_CRITERIA)
     if response.status_code == 200:
-        logging.info("Initial offers check went successfully.")
+        price_criteria = SEARCH_CRITERIA.get("price", {}).get("lte")
+        logging.info(f"Initial offers check went successfully. Price criteria: ${price_criteria}")
         try:
             return response.json()
         except Exception as e:
@@ -108,11 +109,10 @@ while successful_orders < MAX_ORDERS:
     offers = search_gpu().get('offers', [])
     for offer in offers:
         machine_id = offer.get('machine_id')
-        price = offer.get('price', float('inf'))  # Get the price or use infinity if not present
-        if machine_id not in IGNORE_MACHINE_IDS and price <= SEARCH_CRITERIA['price']['lte']:  # Check the price criteria
+        if machine_id not in IGNORE_MACHINE_IDS:
             response = place_order(offer["id"])
             if response.get('success'):
-                logging.info(f"Successfully placed order for machine_id: {machine_id} at a price of ${price:.3f}")
+                logging.info(f"Successfully placed order for machine_id: {machine_id}")
                 successful_orders += 1
                 if successful_orders >= MAX_ORDERS:
                     logging.info("Maximum order limit reached. Exiting...")
@@ -123,6 +123,7 @@ while successful_orders < MAX_ORDERS:
     # Log balance and successful orders count every 5 minutes
     current_time = time.time()
     if current_time - last_balance_log_time >= BALANCE_LOG_INTERVAL:
+        # TODO: Consider fetching the balance again for an updated figure
         logging.info(f"Current balance: ${balance:.2f}")
         logging.info(f"Number of successful orders: {successful_orders}")
         last_balance_log_time = current_time
